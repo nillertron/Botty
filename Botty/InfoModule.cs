c20d467fe927eddaf155670726ccc2261a71f03c
eheu <eheu@libertymail.net>
2019-09-29 21:39:17 +0200

    Add singleton state

--- Botty/InfoModule.cs
+++ Botty/InfoModule.cs
@@ -15,22 +15,23 @@ namespace Botty
         public Task SayAsync([Remainder][Summary("the text to echo")] string echo)
            => ReplyAsync(echo);
         [Group("sample")]
-public class SampleModule : ModuleBase<SocketCommandContext>
+        public class SampleModule : ModuleBase<SocketCommandContext>
         {
             [Command("square")]
             [Summary("Squares a number")]
-            public async Task SquareAsync (
+            public async Task SquareAsync(
                 [Summary("The number to Square")] int num)
             {
                 await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
             }
-                }
- 
+        }
+
     }
     public class NGame : ModuleBase<SocketCommandContext>
     {
-        public int state = 0;
-      public int rand = 0;
+
+        public State state = State.GetState;
+        public int rand = 0;
 
 
         Random rnd = new Random();
@@ -41,22 +42,22 @@ public class SampleModule : ModuleBase<SocketCommandContext>
 
         public async Task gameAsync(int num)
         {
-         
-            if (state == 0)
+
+            if (state.On)
             {
                 await Context.Channel.SendMessageAsync("No game currently running");
             }
-            if (state == 1)
+            if (!state.On)
             {
                 if (num == rand)
                 {
                     await Context.Channel.SendMessageAsync("Number was correct! " + num);
-                    state = 0;
+                    state.On = false;
                     rand = 0;
                 }
                 else if (num > rand)
                 {
-                    await Context.Channel.SendMessageAsync(num + " Was too high ! guess again with !Guess (number) " );
+                    await Context.Channel.SendMessageAsync(num + " Was too high ! guess again with !Guess (number) ");
                 }
                 else if (num < rand)
                 {
@@ -71,17 +72,17 @@ public class SampleModule : ModuleBase<SocketCommandContext>
         [Summary("Ngame")]
         public async Task guessAsync()
         {
-            state = 1;
+            state = State.GetState;
             rand = rnd.Next(1, 100);
 
             await Context.Channel.SendMessageAsync("Game in session! Type !Guess number to beat thef game!");
         }
-
-
-
-
-
-
     }
 
+    public class State
+    {
+        public bool On { get; set; }
+        public static State GetState => new State();
+        private State() { }
+    }
 }
