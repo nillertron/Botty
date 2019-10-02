2b5c345f612e50cb93e8b136be1e211b2ea82ce0
Nichlas <nichlaschristensen@live.dk>
2019-10-02 23:18:34 +0200

    Tilføjet Poll funktion til bot (full funktiolnelt, skal polishes og sættes kommentarer på + oprettes i DB) Orker ikke mere i dag så formattere osv senere. :D

---
+++ Botty/Poll/DbConnect.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using System.Data.SqlClient;
+
+
+
+namespace Botty
+{
+    public class DbConnect : ModuleBase<SocketCommandContext>
+    {
+        const string connectionString = "Data Source=TOKEN;Initial Catalog=Botty;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        #region Poll db
+
+        //Indsæt poll question
+        public async Task indsaetPollQ(string PollQ)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand command = new SqlCommand("Insert into [Question] ([Q], [IsActive]) values ('" + PollQ + "', '1')"))
+                        {
+                            command.Connection = conn;
+                            command.ExecuteNonQuery();
+
+                        }
+
+                        conn.Close();
+
+                    }
+                });
+            }
+            catch (SqlException e)
+            {
+                await Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+        }
+        //Indsætter til Answer i database
+        public async Task indsaetPollA(List<PollAnswers> poll)
+        {
+            int i = 1;
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        foreach (var pollAnswer in poll)
+                        {
+                            using (SqlCommand command = new SqlCommand("Insert into [Answer] ([A], [votes], [Qnum], [Number]) values ('" + pollAnswer.Answer + "', '0', '" + pollAnswer.QId + "','" + i + "')"))
+                            {
+                                command.Connection = conn;
+                                command.ExecuteNonQuery();
+                                i++;
+                            }
+                        }
+                        conn.Close();
+
+                    }
+                });
+            }
+            catch (SqlException e)
+            {
+                await Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+        }
+        //Henter nuværende aktive pollid
+        public int getPollId()
+        {
+            int id = 0;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlDataReader dataReader;
+
+
+                    using (SqlCommand command = new SqlCommand("Select Qnum from question where IsActive = '1'", conn))
+                    {
+                        dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            id = ((int)dataReader.GetValue(0));
+                        }
+
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+
+            return id;
+
+        }
+        public int getActive()
+        {
+            int id = 0;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlDataReader dataReader;
+
+
+                    using (SqlCommand command = new SqlCommand("Select IsActive from question where IsActive = '1'", conn))
+                    {
+                        dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            id = (Convert.ToInt32(dataReader.GetValue(0)));
+                        }
+
+
+                    }
+                }
+
+            }
+            catch (SqlException e)
+            {
+                Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+            return id;
+
+        }
+        public string CheckUser(string user)
+        {
+            int qId = getPollId();
+            var testuser = "";
+
+            try
+            {
+                //Først henter Qnum for at identificere hvilket spørgsmål
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlDataReader dataReader;
+
+                    using (SqlCommand command = new SqlCommand("Select distinct [user] from vote where Qnum='" + qId + "' and [user]='" + user + "'", conn))
+                    {
+                        dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            testuser = (dataReader.GetValue(0).ToString());
+                        }
+                        conn.Close();
+                    }
+                }
+                return testuser;
+            }
+            catch (SqlException e)
+            {
+                Context.Channel.SendMessageAsync(e.ToString());
+                return e.ToString();
+            }
+
+
+
+
+
+        }
+
+        public string GetQuestion(string question)
+        {
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlDataReader dataReader;
+
+
+                    using (SqlCommand command = new SqlCommand("Select Q from question where IsActive = '1'", conn))
+                    {
+                        dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            return question = dataReader.GetValue(0).ToString();
+                        }
+
+
+                    }
+                }
+            }
+
+            catch (SqlException e)
+            {
+                Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+
+            return question;
+
+
+        }
+        public List<PollAnswersFromDb> GetAnswer(List<PollAnswersFromDb> poll)
+        {
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlDataReader dataReader;
+
+
+
+
+                    using (SqlCommand command = new SqlCommand("Select Anum, A, Number, Votes from Answer " +
+                        "inner join question on question.Qnum = Answer.Qnum AND question.IsActive = 1 ", conn))
+                    {
+                        dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            poll.Add(new PollAnswersFromDb(Convert.ToInt32(dataReader.GetValue(0)), dataReader.GetValue(1).ToString(),
+                                Convert.ToInt32(dataReader.GetValue(2)), Convert.ToInt32(dataReader.GetValue(3))));
+                        }
+
+
+                    }
+                }
+            }
+
+            catch (SqlException e)
+            {
+                Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+
+
+
+            return poll;
+        }
+        public async Task indsaetVote(int qnum, string user, int anum)
+        {
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+
+                        using (SqlCommand command = new SqlCommand("Insert into [Vote] ([Qnum], [user], [anum]) " +
+                            "values " +
+                            "('" + qnum + "', '" + user + "', '" + anum + "')" +
+                            "update answer SET votes = votes + 1 where qnum = '" + qnum + "' AND number = '" + anum + "'"))
+                        {
+                            command.Connection = conn;
+                            command.ExecuteNonQuery();
+
+                        }
+
+                        conn.Close();
+
+                    }
+                });
+            }
+            catch (SqlException e)
+            {
+                await Context.Channel.SendMessageAsync(e.ToString());
+
+            }
+        }
+
+        public async Task StopPoll()
+        {
+            await Task.Run(() =>
+            {
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand command = new SqlCommand("Update Question set IsActive = '0' ", conn))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ Botty/Poll/Poll.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Botty
+{
+    public class Poll : ModuleBase<SocketCommandContext>
+    {
+        public Polls newPoll = Polls.newPoll;
+
+
+        //Starter en poll (sætter newPollStarted = true, så vi kan hoppe ned i vores næste metode)
+        [Command("StartPoll")]
+        public async Task StartPoll()
+        {
+            TjekActive();
+
+            if (newPoll.newPollStarted || newPoll.PollActive)
+            {
+                await Context.Channel.SendMessageAsync("A poll is active already!");
+            }
+            else
+            {
+                newPoll.newPollStarted = true;
+                await Context.Channel.SendMessageAsync("Poll started! Type !PollQ QUESTION TO ASK IN POLL");
+            }
+
+
+        }
+
+
+        //tilføjer question til databasen
+        [Command("PollQ")]
+        public async Task OpretPollQ([Remainder]string pollQ)
+        {
+            await ReplyAsync(pollQ);
+
+            if (!newPoll.newPollStarted)
+            {
+                await Context.Channel.SendMessageAsync("No new poll is started");
+            }
+            else
+            {
+                DbConnect db = new DbConnect();
+                await db.indsaetPollQ(pollQ);
+                await Context.Channel.SendMessageAsync("Question registered, start adding answer options with !PollA ");
+                newPoll.newPollStarted = false;
+                newPoll.PollAddA = true;
+            }
+        }
+        //Skaber linjer til listen med svar
+        [Command("PollA")]
+        public async Task OpretPollA([Remainder]string pollA)
+        {
+            if (!newPoll.PollAddA)
+            {
+                await Context.Channel.SendMessageAsync("Create a poll question first or wait for current poll to finish!");
+            }
+            else
+            {
+
+
+                DbConnect db = new DbConnect();
+                int id = db.getPollId();
+                newPoll.poll.Add(new PollAnswers(id, pollA));
+                await Context.Channel.SendMessageAsync("Answer registered, continue adding answers or post the poll !PollPost ");
+
+            }
+        }
+        // Uploader pollens svar til db
+        [Command("PollPost")]
+        public async Task PollPost()
+        {
+            if (!newPoll.PollAddA)
+            {
+                await Context.Channel.SendMessageAsync("Create a poll question first or wait for current poll to finish!");
+            }
+            else
+            {
+                DbConnect db = new DbConnect();
+                await db.indsaetPollA(newPoll.poll);
+
+                await Context.Channel.SendMessageAsync("Poll is active! ");
+                newPoll.poll.Clear();
+                newPoll.PollAddA = false;
+                newPoll.PollActive = true;
+            }
+        }
+        //Udskriver den nuværende Poll
+        [Command("ActivePoll")]
+        public async Task PollActive()
+        {
+            TjekActive();
+
+            if (newPoll.PollActive)
+            {
+                List<PollAnswersFromDb> pollAnswer = new List<PollAnswersFromDb>();
+
+                DbConnect db = new DbConnect();
+                var question = "";
+                question = db.GetQuestion(question);
+                pollAnswer = db.GetAnswer(pollAnswer);
+                await Context.Channel.SendMessageAsync("***" + question + "***");
+                await Context.Channel.SendMessageAsync("-------------");
+
+                foreach (var answer in pollAnswer)
+                {
+                    await Context.Channel.SendMessageAsync($"Valg :***{answer.Nummer}*** \n {answer.Answer} - ***{answer.Votes}***\n-------------");
+
+
+                }
+                await Context.Channel.SendMessageAsync("Add your !Vote now :)");
+
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("No poll active, make one by typing !Startpoll");
+            }
+        }
+        [Command("vote")]
+        public async Task Vote(int vote)
+        {//videre herfra
+            var user = Context.User.Username.ToString();
+
+            bool harStemt = TjekUser(user);
+            TjekActive();
+            if (harStemt)
+            {
+                await Context.Channel.SendMessageAsync(user + " you have already voted, shame on you!");
+
+            }
+
+            else if (newPoll.PollActive)
+            {
+                DbConnect db = new DbConnect();
+                int qnr = db.getPollId();
+                await db.indsaetVote(qnr, user, vote);
+                await Context.Channel.SendMessageAsync("You voted on number " + vote + " gj !");
+
+
+
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("No poll active");
+            }
+        }
+        [Command("StopPoll")]
+        public async Task StopPoll()
+        {
+            if (newPoll.PollActive)
+            {
+                DbConnect db = new DbConnect();
+                await db.StopPoll();
+                newPoll.PollActive = false;
+                await Context.Channel.SendMessageAsync("Poll stopped");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("No poll to stop");
+            }
+        }
+
+        private void TjekActive()
+        {
+            //Tjek om der er en poll active i db:
+            DbConnect db = new DbConnect();
+            int i = 0;
+
+
+            i = db.getActive();
+
+            if (i > 0)
+            {
+                newPoll.PollActive = true;
+
+            }
+
+
+        }
+        private bool TjekUser(string user)
+        {
+            DbConnect db = new DbConnect();
+            string testUser = "";
+            testUser = db.CheckUser(user);
+
+
+
+
+            //findes brugeren return true
+            if (testUser == user)
+            {
+                return true;
+            }//ellers return false
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+
+}
+
+public class Polls
+{
+
+
+    public bool newPollStarted { get; set; } = false;
+    public bool PollAddA { get; set; } = false;
+    public bool PollActive { get; set; } = false;
+    //En instaniseret liste til at gemme svar i
+    public List<PollAnswers> poll = new List<PollAnswers>();
+
+
+    public static Polls newPoll { get; } = new Polls();
+    public Polls()
+    {
+    }
+
+}
+//Gemmer i listen til DB
+public class PollAnswers
+{
+    public int QId { get; }
+    public string Answer { get; }
+
+
+    public PollAnswers(int qId, string answer)
+    {
+        QId = qId;
+        Answer = answer;
+
+    }
+}
+//Skal bruges til listen fra DB (var lige nemmere end at ændre koden :D)
+public class PollAnswersFromDb
+{
+    public int AId { get; }
+    public string Answer { get; }
+
+    public int Nummer { get; }
+    public int Votes { get; }
+
+    public PollAnswersFromDb(int aId, string answer, int nummer, int votes)
+    {
+        AId = aId;
+        Answer = answer;
+        Nummer = nummer;
+        Votes = votes;
+
+    }
+}
+
